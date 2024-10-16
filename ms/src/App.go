6f6f85df742672fbb52ccc01ff9f7bcb4944ebce
Diego Martínez <diego.martinez01@alumnos.ucn.cl>
2024-10-16 11:20:25 -0300

    a

--- ms/src/App.go
+++ ms/src/App.go
@@ -1,1 +1,8 @@
 package App
+
+func main() {
+	conexion, err := connection.connectToDataBase()
+	if err != nil{
+		println("error")
+	}
+}
--- ms/src/connection/connection.go
+++ ms/src/connection/connection.go
@@ -1,5 +1,32 @@
 package connection
 
-func connectToDataBase() {
+import (
+	"context"
+	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connectToDataBase() (*mongo.Client, error) {
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://diegomartinezzr:<db_password>@cluster.cl2lp.mongodb.net/?retryWrites=true&w=majority&appName=cluster").SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	return client, nil
 }
