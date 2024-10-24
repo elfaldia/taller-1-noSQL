bf934b0a48e1c050de2b712632cd0f058cbfb26a
elfaldia <diego.17rojas2002@gmail.com>
2024-10-24 13:46:45 -0300

    Unidad y Query armado

---
+++ internal/repository/unidad_repository.go
@@ -0,0 +1,101 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/elfaldia/taller-noSQL/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UnidadRepository interface {
+	FindAll() ([]model.Unidad, error)
+	FindByIdCurso(unidadId string) ([]model.Unidad, error)
+	InsertOne(unidad model.Unidad) (model.Unidad, error)
+	InsertMany(unidades []model.Unidad) ([]model.Unidad, error)
+}
+
+type UnidadRepositoryImpl struct {
+	Collection *mongo.Collection
+	Ctx        *context.Context
+}
+
+func NewUnidadRepositoryImpl(unidadCollection *mongo.Collection) UnidadRepository {
+	return &UnidadRepositoryImpl{Collection: unidadCollection}
+}
+
+// Obtener todos las unidades
+func (u *UnidadRepositoryImpl) FindAll() ([]model.Unidad, error) {
+	unidades, err := u.Collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		return nil, err
+	}
+
+	var resultado []model.Unidad
+	err = unidades.All(context.TODO(), &resultado)
+	if err != nil {
+		return nil, err
+	}
+	return resultado, nil
+}
+
+// Obtener una unidad por un ID
+func (u *UnidadRepositoryImpl) FindByIdCurso(CursoId string) ([]model.Unidad, error) {
+
+	var resultado []model.Unidad
+
+	objectID, err := primitive.ObjectIDFromHex(CursoId)
+	if err != nil {
+		return nil, err
+	}
+
+	unidades, err := u.Collection.Find(context.TODO(), bson.M{"id_curso": objectID})
+	if err != nil {
+		return nil, err
+	}
+
+	defer unidades.Close(context.TODO())
+
+	err = unidades.All(context.TODO(), &resultado)
+	if err != nil {
+		return nil, err
+	}
+
+	return resultado, nil
+}
+
+// Insertar una unidad
+func (u *UnidadRepositoryImpl) InsertOne(unidad model.Unidad) (model.Unidad, error) {
+
+	insertarUnidad, err := u.Collection.InsertOne(context.TODO(), unidad)
+	if err != nil {
+		return unidad, err
+	}
+
+	unidad.Id = insertarUnidad.InsertedID.(primitive.ObjectID)
+
+	return unidad, nil
+}
+
+// Ingresar muchas unidades en foramto de arrays de json
+func (u *UnidadRepositoryImpl) InsertMany(unidades []model.Unidad) ([]model.Unidad, error) {
+	var unidadesInsertados []model.Unidad
+	var documentos []interface{}
+
+	for _, unidad := range unidades {
+		documentos = append(documentos, unidad)
+	}
+
+	resultado, err := u.Collection.InsertMany(context.TODO(), documentos)
+	if err != nil {
+		return nil, err
+	}
+
+	for i, id := range resultado.InsertedIDs {
+		unidades[i].Id = id.(primitive.ObjectID)
+		unidades[i].IdCurso = id.(primitive.ObjectID)
+		unidadesInsertados = append(unidadesInsertados, unidades[i])
+	}
+	return unidadesInsertados, nil
+}
---
+++ internal/request/unidad_request.go
@@ -0,0 +1,11 @@
+package request
+
+type CrearUnidadRequest struct {
+	Nombre  string `json:"nombre" bson:"nombre"`
+	Indice  int    `json:"indice" bson:"indice"`
+	IdCurso string `json:"id_curso" bson:"id_curso"`
+}
+
+type CrearUnidadesRequest struct {
+	Data []CrearUnidadRequest `json:"data" validate:"required"`
+}
